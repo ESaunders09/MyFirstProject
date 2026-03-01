37c5695e2d3f400c253cfe62ee0a0457a3c798fa
Ethan Saunders <ethansaunders1209@gmail.com>
2026-03-01 21:22:33 +0000

    Add random password generation

--- password_generator.py
+++ password_generator.py
@@ -1,3 +1,17 @@
+import random
+
+# Define characters that can be in the password
+letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "0123456789"
+symbols = "!@#$%^&()_+-="
+all_chars = letters + numbers + symbols # Combine everything
+
 # Ask the user how long they want the password
 length = int(input("How long do you want your password to be?"))
 print("Password length set to:", length)
+
+# Generate the random password
+password = ""
+for i in range(length):
+    password += random.choice(all_chars)
+print("Your password is:", password)
